e4ea7a979c7f699433e690def88dd45d692ad53f
joestephens <joe.stephens@live.co.uk>
2016-07-18 11:20:39 +0100

    Add photo only shows for logged in user

--- spec/features/photos_spec.rb
+++ spec/features/photos_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'photos' do
   context 'no photos have been added' do
     before do
-      visit '/photos'
+      test_sign_up
     end
 
     scenario 'should display a button to upload a new photo' do
@@ -14,6 +14,13 @@ feature 'photos' do
     scenario '\'Add New Photo\' button should take user to new photo page' do
       click_button 'Add New Photo'
       expect(current_path).to eq '/photos/new'
+    end
+  end
+
+  context 'not signed in' do
+    scenario 'shouldn\'t display a button to upload a new photo' do
+      visit '/photos'
+      expect(page).not_to have_button 'Add New Photo'
     end
   end
 
